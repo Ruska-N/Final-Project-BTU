13fc27702626718d99ad03da1dbd63af1b79668a
Mariam <mariebukhaidze@gmail.com>
2025-11-06 19:49:56 +0400

    feat: Scaffold components, services, interfaces, and mock data file

---
+++ e-Commerce/src/app/components/side-cart/side-cart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-cart',
+  imports: [],
+  templateUrl: './side-cart.component.html',
+  styleUrl: './side-cart.component.css'
+})
+export class SideCartComponent {
+
+}
---
+++ e-Commerce/src/app/pages/gift-card-page/gift-card-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gift-card-page',
+  imports: [],
+  templateUrl: './gift-card-page.component.html',
+  styleUrl: './gift-card-page.component.css'
+})
+export class GiftCardPageComponent {
+
+}
