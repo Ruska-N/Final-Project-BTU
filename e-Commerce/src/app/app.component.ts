c47e9f9272123d49e12caef20e78fbc00594ae8c
Mariam <mariebukhaidze@gmail.com>
2025-11-07 14:57:49 +0400

    working on Footer

--- e-Commerce/src/app/app.component.ts
+++ e-Commerce/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// 1. დააიმპორტე შენი 3 "მთავარი" კომპონენტი
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideCartComponent } from './components/side-cart/side-cart.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SideCartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'e-Commerce';
--- e-Commerce/src/app/components/footer/footer.component.ts
+++ e-Commerce/src/app/components/footer/footer.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  contactEmail = 'info@mysite.com';
+  exampleEmail = 'example@mysite.com';
 
+
+  submitted = false;
+
+
+  subscribeForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    subscribeCheck: new FormControl(false, [Validators.requiredTrue]),
+  });
+
+  get email() {
+    return this.subscribeForm.get('email');
+  }
+
+  get subscribeCheck() {
+    return this.subscribeForm.get('subscribeCheck');
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.subscribeForm.valid) {
+      console.log('Form Submitted!', this.subscribeForm.value);
+      alert('Thank you for subscribing!');
+
+
+      this.subscribeForm.reset();
+      this.submitted = false;
+    }
+  }
 }
