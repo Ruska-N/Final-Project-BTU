02728634d0e12e05d7c9b7f2c9fea7e858a2fce9
Mariam <mariebukhaidze@gmail.com>
2025-11-10 18:01:41 +0400

    feat(contact): add contact page and form

--- e-Commerce/src/app/app.routes.ts
+++ e-Commerce/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ShopPageComponent } from './pages/shop-page/shop-page.component';
 import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
 import { GiftCardPageComponent } from './pages/gift-card-page/gift-card-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component'; // Импортируйте компонент
 
 export const routes: Routes = [
   {
@@ -18,6 +19,11 @@ export const routes: Routes = [
     path: 'gift-card',
     component: GiftCardPageComponent,
     title: 'eGift Card',
+  },
+  {
+    path: 'contact', 
+    component: ContactPageComponent,
+    title: 'Contact Us',
   },
   {
     path: '**',
--- e-Commerce/src/app/pages/contact-page/contact-page.component.ts
+++ e-Commerce/src/app/pages/contact-page/contact-page.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-page.component.html',
-  styleUrl: './contact-page.component.css'
+  styleUrls: ['./contact-page.component.css'],
 })
 export class ContactPageComponent {
+  contactForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl(''),
+    message: new FormControl('', [Validators.required]),
+  });
 
+  submitted = false;
+
+  get firstName() {
+    return this.contactForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.contactForm.get('lastName');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.contactForm.valid) {
+      console.log('Form Submitted!', this.contactForm.value);
+      alert('Your message has been sent!');
+      this.contactForm.reset();
+      this.submitted = false;
+    }
+  }
 }
