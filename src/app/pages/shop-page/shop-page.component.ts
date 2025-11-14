6eebdfbd1667deb9b9864bb2debd5a804af0d63d
Mariam <mariebukhaidze@gmail.com>
2025-11-14 13:48:06 +0400

    corrected navigation to about

--- src/app/pages/shop-page/shop-page.component.ts
+++ src/app/pages/shop-page/shop-page.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Product } from '../../interfaces/product.interface';
 import { ProductService } from '../../services/product.service';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shop-page',
@@ -14,12 +15,31 @@ import { ProductCardComponent } from '../../components/product-card/product-card
 export class ShopPageComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
-
       this.products = data;
     });
+
+    this.route.fragment.subscribe((fragment) => {
+      if (fragment) {
+        setTimeout(() => this.scrollToElement(fragment), 100);
+      }
+    });
+  }
+
+  private scrollToElement(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
   }
 }
